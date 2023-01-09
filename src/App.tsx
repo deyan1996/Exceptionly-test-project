45d4b8f37bdf8cd943e840b4ff9a4c339ebcd462
unknown <dejanpetrovickgy@gmail.com>
2023-01-09 05:59:50 +0100

    fix: app.tsx file jwtprovider updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
+import JwtProvider from './components/Auth/JwtProvider';
 import { ReactElement } from 'react';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
-import LoadingScreen from './components/LoadingScreen';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 import routes, { renderRoutes } from './routes';
-import JwtProvider from './components/Auth/JwtProvider';
 
 const history = createBrowserHistory();
 
 function App() : ReactElement {
   return (
     <Provider store={store}>
-      <PersistGate loading={<LoadingScreen />} persistor={persistor}>
-        <Router history={history}>
-          <JwtProvider>
-            {renderRoutes(routes)}
-          </JwtProvider>
-        </Router>
-      </PersistGate>
+      <Router history={history}>
+        <JwtProvider>
+          {renderRoutes(routes)}
+        </JwtProvider>
+      </Router>
     </Provider>
   );
 }
