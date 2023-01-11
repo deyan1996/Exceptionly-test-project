16335cb311dac97c8f9a10333598c02050d7d3d8
Smart Owl <dejanpetrovickgy@gmail.com>
2023-01-11 22:19:20 +0100

    feat: add google firebase provider

--- src/firebase/index.tsx
+++ src/firebase/index.tsx
@@ -1,19 +1,9 @@
 import 'firebase/compat/auth';
-import React, {
-  createContext,
-  useContext,
-  useState
-} from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  query, where
-} from 'firebase/firestore';
-import firebase from 'firebase/compat/app';
 import { AuthError } from 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import { getFirestore, collection, addDoc, getDocs, query, where } from 'firebase/firestore';
 
 import { FIREBASE_CONFIG } from '../util/config';
 
@@ -36,6 +26,141 @@ type FirebaseStateType = {
 
 firebase.initializeApp(FIREBASE_CONFIG);
 const FirebaseContext = createContext<FirebaseStateType | undefined>(undefined);
+
+function FirebaseProvider({ children }: FirebaseProviderPropsType) {
+  const db = getFirestore();
+  const auth = firebase.auth();
+  const userRef = collection(db, 'users');
+
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  googleProvider.setCustomParameters({ prompt: 'select_account' });
+
+  const microsoftProvider = new firebase.auth.OAuthProvider('microsoft.com');
+
+  const [user, setUser] = useState<firebase.User | null>(null);
+  const [token, setToken] = useState<string | undefined>(undefined);
+  const [error, setError] = useState<string | null>(null);
+  const [isFetchingUser, setIsFetchingUser] = useState(true);
+
+  auth.onAuthStateChanged(async (user) => {
+    if (user) {
+      setUser(user);
+      const token = await user.getIdToken();
+      setToken(token);
+    } else {
+      setUser(null);
+      setToken(undefined);
+    }
+
+    setIsFetchingUser(false);
+  });
+
+  const registerWithCredentials = async (email: string, password: string) => {
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+      if (!user) {
+        setError('No user was found');
+        return;
+      }
+
+      await addDoc(userRef, {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: 'local',
+        email: user.email
+      });
+    } catch (error) {
+      const e = error as AuthError;
+      const errorCode = e.code;
+      const errorMessage = e.message;
+      if (errorCode === 'auth/weak-password') {
+        setError('The password is too weak.');
+      } else if (errorCode === 'auth/network-request-failed') {
+        setError('Request failed. Check your internet connection and try again.');
+      } else {
+        setError(errorMessage);
+      }
+      console.log(e);
+    }
+  };
+
+  const loginWithCredentials = async (email: string, password: string) => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      const e = error as AuthError;
+      const errorCode = e.code;
+      const errorMessage = e.message;
+
+      console.log(errorCode);
+
+      if (errorCode === 'auth/wrong-password') {
+        setError('Wrong password.');
+      } else if (errorCode === 'auth/user-not-found') {
+        setError('User not found');
+      } else if (errorCode === 'auth/network-request-failed') {
+        setError('Request failed. Check your internet connection and try again.');
+      } else {
+        setError(errorMessage);
+      }
+      console.log(e);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      const { user } = await auth.signInWithPopup(googleProvider);
+
+      if (!user) {
+        setError('No user was found');
+        return;
+      }
+
+      const result = await getDocs(query(userRef, where('uid', '==', user.uid)));
+
+      if (result.empty) {
+        await addDoc(collection(db, 'users'), {
+          uid: user.uid,
+          name: user.displayName,
+          authProvider: 'google',
+          email: user.email
+        });
+      }
+    } catch (error) {
+      const e = error as AuthError & { email: string };
+      const errorCode = e.code;
+      const email = e.email;
+      const errorMessage = e.message;
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        auth.fetchSignInMethodsForEmail(email).then(function (providers) {
+          // figure out what to do here later
+          console.log(providers);
+        });
+      } else if (errorCode === 'auth/network-request-failed') {
+        setError('Request failed. Check your internet connection and try again.');
+      } else {
+        setError(errorMessage);
+      }
+    }
+  };
+
+  return (
+    <FirebaseContext.Provider
+      value={{
+        user,
+        error,
+        token,
+        setError,
+        isFetchingUser,
+        registerWithCredentials,
+        loginWithCredentials,
+        signInWithGoogle,
+      }}>
+      {children}
+    </FirebaseContext.Provider>
+  );
+}
 
 function useFirebase() {
   const context = useContext(FirebaseContext);
